d0275a3a3148369b6fa55de5b0b7fc6d331e9813
Salis Salman <112612817+salissalmann@users.noreply.github.com>
2024-04-28 20:00:09 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import PDFPage from './PDFPage';
 
 const App = () => {
